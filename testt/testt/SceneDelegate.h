d2d70e59c6608565e5c71408ccab1dd3ff60a161
XiaoKai <767713227@qq.com>
2019-12-20 15:43:08 +0800

    no message

---
+++ testt/testt/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  testt
+//
+//  Created by 王恒凯 on 2019/12/20.
+//  Copyright © 2019 Defence. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ testt/testt/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  testt
+//
+//  Created by 王恒凯 on 2019/12/20.
+//  Copyright © 2019 Defence. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
